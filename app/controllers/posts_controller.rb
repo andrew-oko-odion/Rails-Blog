a0362115cd86c84889596320d35067b3ebb2d713
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-08 09:59:14 +0100

    Allow User to be able to create Articles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,30 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: 'DESC')
   end
 
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    respond_to do |format|
+      @post = Post.new(post_params)
+      if @post.save!
+        format.html { redirect_to @post, notice: 'Article created successfully' }
+        format.js
+        format.json { render json: @post, status: :ok }
+      else
+        format.html { render :new, error: @post.errors}
+        format.js
+        format.json { render json: @post.errors}
+      end
+    end
+  end
+  
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
