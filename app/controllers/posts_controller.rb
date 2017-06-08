d3c60c69fdf6c6a81fe9258d73755d224ad0bc92
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-08 11:27:22 +0100

    Add support for Delete and Edit of post article

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,8 +28,30 @@ class PostsController < ApplicationController
   end
   
   def edit
+    @post = Post.find params[:id]
   end
 
+  def update
+    @post = Post.find params[:id]
+    post = @post.update_attributes!(post_params)
+    respond_to do |format|
+      if post 
+        format.html {redirect_to @post, notice: 'Updated Post Successfully' }
+        format.js
+        format.json { render json: @post, status: :ok }                                 
+      else
+        format.html {redirect_to :edit, notice: post.errors }
+        format.js
+        format.json { render json: post.errors }
+      end
+    end
+  end
+
+  def destroy
+    @post = Post.find params[:id]
+    @post.destroy!
+  end
+  
   private
   def post_params
     params.require(:post).permit(:title, :body)
